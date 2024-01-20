fc2b5e1f3717944abdded1a826a4eaa20c0419d1
javi <55107141+javierandres-dev@users.noreply.github.com>
2024-01-19 21:27:25 -0500

    training

--- my-app/src/app/components/tutorial-page/tutorial-page.component.ts
+++ my-app/src/app/components/tutorial-page/tutorial-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ParentComponent } from '../parent/parent.component';
+import { Employee } from '../../interfaces/employee';
 
 @Component({
   selector: 'app-tutorial-page',
@@ -8,4 +9,31 @@ import { ParentComponent } from '../parent/parent.component';
   templateUrl: './tutorial-page.component.html',
   styleUrl: './tutorial-page.component.css',
 })
-export class TutorialPageComponent {}
+export class TutorialPageComponent {
+  city: string = 'New York';
+
+  isLoggedIn: boolean = false;
+
+  frameworks: string[] = ['angular', 'react', 'svelte', 'vue'];
+  students: any[] = [
+    { id: 0, name: 'Huey' },
+    { id: 1, name: 'Dewey' },
+    { id: 2, name: 'Louie' },
+  ];
+
+  isEditable: boolean = true;
+
+  handleClick() {
+    console.log('handle click works');
+  }
+
+  handleMouseover() {
+    console.log('mouseover works');
+  }
+
+  myEmployee: Employee = {
+    id: '1',
+    firstName: 'Mike',
+    lastName: 'Smith',
+  };
+}
